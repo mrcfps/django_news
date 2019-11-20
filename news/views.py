f32f81013952bb4af23487ef6ae82fe79aa8dcb6
mRcfps <1402491442@qq.com>
2019-11-20 22:41:00 +0800

    理解模型：和数据库的联动

--- news/views.py
+++ news/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def index(request):
-    context = {
-        'news_list': [
-            {
-                "title": "图雀写作工具推出了新的版本",
-                "content": "随随便便就能写出一篇好教程，真的很神奇",
-            },
-            {
-                "title": "图雀社区正式推出快速入门系列教程",
-                "content": "一杯茶的功夫，让你快速上手，绝无担忧",
-            },
-        ]
-    }
-
+    context = { 'news_list': Post.objects.all() }
     return render(request, 'news/index.html', context=context)
